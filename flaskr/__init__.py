d7755a1112960c34fa910c9025faf12cefc898f9
AWAWAW123 <610843701@qq.com>
2021-06-30 08:58:02 +0000

    1

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -25,7 +25,7 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/1313')
+    @app.route('/')
     def hello():
         return 'Hello, World!'
     
